8a693822f0872e3cc7469ecfecc6dd6d5dd181ad
qafro <qafrogce@gmail.com>
2019-11-28 20:44:57 +0000

    added source_arn

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda.function_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.id}/*"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
   
 }
 
@@ -81,8 +81,6 @@ resource "aws_api_gateway_integration" "integration" {
   http_method = "${aws_api_gateway_method.method.http_method}"
   type        = "AWS_PROXY"
   uri         = "${aws_lambda_function.lambda.invoke_arn}"
-          
-
 }
 
 resource "aws_api_gateway_deployment" "deployment" {

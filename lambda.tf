52e07206bc8e2341f2121a9bdaf7f5cb9b0889eb
qafro <qafrogce@gmail.com>
2019-11-28 15:05:49 +0000

    updated lambda_function.lambda_handler

--- lambda.tf
+++ lambda.tf
@@ -30,9 +30,9 @@ EOF
 resource "aws_lambda_function" "web_lambda" {
   count            = "${var.enabled ? 1 : 0}"
   filename         = "${data.archive_file.zip.output_path}"
-  function_name    = "hellocloudgurus"
+  function_name    = "lambda_function"
   role             = "${aws_iam_role.lambda_role.arn}"
-  handler          = "hellocloudgurus.lambda_handler"
+  handler          = "lambda_function.lambda_handler"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("hellocloudgurus.py"))}"

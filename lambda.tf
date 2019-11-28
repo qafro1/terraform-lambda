2fed5b852757a54664eccdbc5a883ac62520a061
qafro <qafrogce@gmail.com>
2019-11-28 14:08:58 +0000

    added data source

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,11 @@
+# Archive the code or project that we want to run
+data "archive_file" "zip" {
+  type = "zip"
+  source_file = "hellocloudgurus.py"
+  output_path = "hellocloudgurus.zip"
+  
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
 
@@ -20,18 +28,18 @@ EOF
 
 resource "aws_lambda_function" "web_lambda" {
   filename         = "hellocloudgurus.py"
-  function_name    = "lambda_function_py"
+  function_name    = "${data.archive_file.zip.output_path}"
   role             = "${aws_iam_role.lambda_role.arn}"
   handler          = "lambda_function.lambda_handler"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("hellocloudgurus.py"))}"
-  source_code_hash = "${filebase64sha256("~/hellocloudgurus.py")}"
+  source_code_hash  = "${base64sha256(data.archive_file.zip.output_path)}"
   runtime          = "python3.6"
 
   environment {
     variables = {
-      dev = "demo"
+      #key = "value"
     }
   }
 }
--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket_object" "s3lambda" {
-  bucket = "s3lambda"
-  key    = "s3key"
-  source = "/Users/gof/git/lambda/hellocloudgurus.py"
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("/Users/gof/git/lambda/hellocloudgurus.py"))}"
-  etag = "${filemd5("/Users/gof/git/lambda/hellocloudgurus.py")}"
-}

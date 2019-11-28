b00107bd0a917ef6115bd7d531bf8fea9529c1e6
qafro <qafrogce@gmail.com>
2019-11-28 21:46:38 +0000

    recap

--- main.tf
+++ main.tf
@@ -1,3 +1,44 @@
+
+# ADD trigger API GATEWAY to lambda function
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "api"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
+  type        = "AWS_PROXY"
+  uri         = "${aws_lambda_function.lambda.invoke_arn}"
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = ["aws_api_gateway_integration.integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "test"
+
+  variables = {
+    "answer" = "42"
+  }
+}
+
 # Specify the provider and access details
 # Upload file for lambda 
 provider "archive" {}
@@ -50,48 +91,8 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda.function_name}"
   principal     = "apigateway.amazonaws.com"
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
   
 }
-
-
-# ADD trigger API GATEWAY to lambda function
-
-resource "aws_api_gateway_rest_api" "api" {
-  name        = "api"
-  description = "This is my API for demonstration purposes"
-}
-
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "test"
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_resource.resource.id}"
-  http_method = "${aws_api_gateway_method.method.http_method}"
-  type        = "AWS_PROXY"
-  uri         = "${aws_lambda_function.lambda.invoke_arn}"
-  integration_http_method = "POST"
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = ["aws_api_gateway_integration.integration"]
-
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "test"
-
-  variables = {
-    "answer" = "42"
-  }
-}
-

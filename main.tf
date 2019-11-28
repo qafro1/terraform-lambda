f4591f53a500a0ff20f425152f2e07db35c7f96a
qafro <qafrogce@gmail.com>
2019-11-28 21:33:23 +0000

    renamed source_arn

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda.function_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
   
 }
 

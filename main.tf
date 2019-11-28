1fbf6411258fff19e3db051fd999163d28e20fb1
qafro <qafrogce@gmail.com>
2019-11-28 17:15:58 +0000

    added API GATEWAY

--- main.tf
+++ main.tf
@@ -45,5 +45,41 @@ resource "aws_lambda_function" "lambda" {
   #}
 }
 
+# ADD trigger API GATEWAY to lambda function
 
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "api"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "methrod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.methrod.http_method}"
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = ["aws_api_gateway_integration.integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "test"
+
+  variables = {
+    "answer" = "42"
+  }
+}
 
